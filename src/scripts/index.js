2bd6a07c0446d79c793f507084c7dc3c99a7f154
afzal-apttus <afqureshi@apttus.com>
2019-02-09 12:33:37 +0530

    Demo code done till closures

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,9 @@
+let app = (function () {
+  let carId = 123;
+  let getId = function () {
+    return carId;
+  };
+  return { getId: getId };
+})();
+console.log(app.getId());
 
-for (let i=0; i<5; i++) { 
-  if (i === 3) 
-    continue;
-  console.log(i);
-}
